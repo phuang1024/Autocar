e992319cec16265e009f302b84a71ef283c70017
Patrick Huang <huangpatrick16777216@gmail.com>
2024-06-19 12:00:21 -0700

    a-

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,50 +1,79 @@
-#include <IBusBM.h>
+//#include <IBusBM.h>
 
 
+// pin order: ena, pul, dir
 class Motors {
 public:
     Motors() {
-        for (int i = 4; i < 8; i++) {
-            pinMode(i, OUTPUT);
-            digitalWrite(i, LOW);
-        }
-        for (int i = 22; i < 30; i++) {
+        for (int i = 2; i <= 7; i++) {
             pinMode(i, OUTPUT);
             digitalWrite(i, LOW);
         }
     }
 
-    void write_dir(int i, int dir) {
-        int pin = 22 + 2 * i;
-        if (i >= 2) {
-            dir = !dir;
-        }
-        digitalWrite(pin, dir);
-        digitalWrite(pin + 1, !dir);
+    void write_ena(bool ena) {
+        digitalWrite(2, ena);
+        digitalWrite(5, ena);
     }
 
-    void write_speed(int i, int speed) {
-        int pin = 4 + i;
-        analogWrite(pin, speed);
+    void write_dir_left(bool dir) {
+        digitalWrite(4, dir);
+    }
+
+    void write_dir_right(bool dir) {
+        digitalWrite(7, dir);
+    }
+
+    void write_dir_both(bool dir) {
+        write_dir_left(dir);
+        write_dir_right(dir);
+    }
+
+    void write_pwm_left(int pwm) {
+        analogWrite(3, pwm);
+    }
+
+    void write_pwm_right(int pwm) {
+        analogWrite(6, pwm);
+    }
+
+    void write_pwm_both(int pwm) {
+        write_pwm_left(pwm);
+        write_pwm_right(pwm);
+    }
+
+    void write_vel_left(int vel) {
+        write_dir_left(vel >= 0);
+        write_pwm_left(abs(vel));
+    }
+
+    void write_vel_right(int vel) {
+        write_dir_right(vel >= 0);
+        write_pwm_right(abs(vel));
+    }
+
+    void write_vel_both(int vel) {
+        write_vel_left(vel);
+        write_vel_right(vel);
     }
 };
 
 
 Motors motors;
-IBusBM ibus;
+//IBusBM ibus;
 
 
 void setup() {
     delay(100);
 
     Serial.begin(9600);
-    ibus.begin(Serial1, 2);
+    //ibus.begin(Serial1, 2);
 
-    for (int i = 0; i < 4; i++) {
-        motors.write_dir(i, true);
-    }
+    int i = 0;
+    motors.write_ena(true);
     while (true) {
         // read RC
+        /*
         uint16_t rx_steering = ibus.readChannel(0);
         uint16_t rx_throttle = ibus.readChannel(2);
         uint16_t rx_enable = ibus.readChannel(4);
@@ -53,8 +82,6 @@ void setup() {
         int speed = map(rx_throttle, 1000, 2000, 0, 255);
         int speed1 = speed + steer;
         int speed2 = speed - steer;
-        Serial.println(rx_steering);
-        Serial.println(steer);
         speed1 = constrain(speed1, 0, 255);
         speed2 = constrain(speed2, 0, 255);
 
@@ -69,7 +96,29 @@ void setup() {
             }
         }
 
-        delay(50);
+        delay(10);
+        */
+
+
+        //motors.write_vel_both(100);
+        //delay(1000);
+        //continue;
+        motors.write_ena(true);
+        delay(1000);
+        motors.write_ena(false);
+        delay(1000);
+        continue;
+
+        while (i < 255) {
+            motors.write_vel_both(i);
+            delay(30);
+            i++;
+        }
+        while (i > -255) {
+            motors.write_vel_both(i);
+            delay(30);
+            i--;
+        }
     };
 }
 
