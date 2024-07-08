0c25da7c0f08ec2acc2b0dd93ecf2477c1291d84
Patrick Huang <huangpatrick16777216@gmail.com>
2024-07-08 09:14:05 -0700

    Client threading.

--- software/model.py
+++ software/model.py
@@ -2,6 +2,7 @@ import math
 import random
 import time
 from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
 
 import cv2
 
@@ -34,6 +35,48 @@ def post_new_data(args, img, label):
     sendobj(conn, {"type": "new_data", "img": img_data, "label": label})
 
 
+def worker_infer(args, objs):
+    while True:
+        if objs["img_rgb"] is not None:
+            img = torch.tensor(objs["img_rgb"]).permute(2, 0, 1).unsqueeze(0).float() / 255
+            pred = objs["model"](img).item()
+            pred = math.tanh(pred)
+            objs["interface"].nn_pred = pred
+            objs["pred"] = pred
+
+        time.sleep(args.infer_ival)
+
+
+def worker_new_data(args, objs):
+    while True:
+        online_enabled = objs["interface"].rc_values[5] > 0.5
+        if online_enabled and objs["img_rgb"] is not None:
+            if random.random() < abs(objs["interface"].rc_values[0] - 0.5) + 0.5:
+                post_new_data(args, objs["img_rgb"], objs["interface"].rc_values[0] * 2 - 1 + objs["pred"])
+            else:
+                time.sleep(1)
+
+
+def worker_new_model(args, objs):
+    while True:
+        online_enabled = objs["interface"].rc_values[5] > 0.5
+        if online_enabled:
+            conn = create_conn(args)
+            sendobj(conn, {"type": "get_model"})
+            model_data = recvobj(conn)["model"]
+            conn.close()
+
+            if model_data is not None:
+                with open("/tmp/model.pt", "wb") as f:
+                    f.write(model_data)
+                try:
+                    objs["model"].load_state_dict(torch.load("/tmp/model.pt", map_location=DEVICE))
+                except Exception as e:
+                    print("Model update failed:", e)
+                else:
+                    print("Model updated.")
+
+
 def train_main(args, interface):
     """
     Main for reinforcement learning client (car).
@@ -43,46 +86,23 @@ def train_main(args, interface):
 
     interface.add_thread(interface.auto_rc)
 
-    last_new_data = 0
-    last_new_model = 0
+    objs = {
+        "model": model,
+        "interface": interface,
+        "pred": 0,
+        "img_rgb": None,
+    }
+
     with depthai.Device(pipeline) as device:
         q_rgb = device.getOutputQueue("rgb")
 
+        threads = []
+        threads.append(Thread(target=worker_infer, args=(args, objs)))
+        threads.append(Thread(target=worker_new_data, args=(args, objs)))
+        threads.append(Thread(target=worker_new_model, args=(args, objs)))
+        for thread in threads:
+            thread.start()
+
         while True:
             img_rgb = read_latest(q_rgb).getCvFrame()
-
-            # Infer model
-            img = torch.tensor(img_rgb).permute(2, 0, 1).unsqueeze(0).float() / 255
-            pred = model(img).item()
-            pred = math.tanh(pred)
-            interface.nn_pred = pred
-            print("pred", pred)
-
-            online_enabled = interface.rc_values[5] > 0.5
-
-            # Check for new data.
-            if online_enabled:# and time.time() - last_new_data > args.new_data_ival:
-                last_new_data = time.time()
-                if random.random() < abs(interface.rc_values[0] - 0.5) + 0.5:
-                    post_new_data(args, img_rgb, interface.rc_values[0] * 2 - 1 + pred)
-
-            # Check for new model.
-            if online_enabled and time.time() - last_new_model > args.new_model_ival:
-                last_new_model = time.time()
-
-                conn = create_conn(args)
-                sendobj(conn, {"type": "get_model"})
-                model_data = recvobj(conn)["model"]
-                conn.close()
-
-                if model_data is not None:
-                    with open("/tmp/model.pt", "wb") as f:
-                        f.write(model_data)
-                    try:
-                        model.load_state_dict(torch.load("/tmp/model.pt", map_location=DEVICE))
-                    except Exception as e:
-                        print("Model update failed:", e)
-                    else:
-                        print("Model updated.")
-
-            time.sleep(args.infer_ival)
+            objs["img_rgb"] = img_rgb
