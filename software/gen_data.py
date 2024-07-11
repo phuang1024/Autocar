9fdecf5ab420b63214d22409e74ace8ba19ceb80
Patrick Huang <huangpatrick16777216@gmail.com>
2024-07-11 13:14:47 -0700

    Test auto rc code.

--- software/gen_data.py
+++ software/gen_data.py
@@ -6,6 +6,7 @@ import time
 from pathlib import Path
 
 import cv2
+import numpy as np
 
 from camera import *
 
@@ -30,27 +31,81 @@ class DataGen:
         self.i += 1
 
 
+def manual_rc(args, interface, wrapper, data_gen):
+    while True:
+        images = wrapper.get()
+
+        if interface.rc_values[5] > 0.5:
+            label = interface.rc_values[0] * 2 - 1
+            data_gen.write(images, label)
+
+        time.sleep(0.01)
+
+
+def self_rc(args, interface, wrapper, data_gen):
+    # Mask for depth_x.
+    mask = np.zeros((args.res, args.res), dtype=np.float32)
+    for y in range(args.res):
+        value = 1 - abs(y - args.res / 2) / (args.res / 2)
+        mask[y] = value
+
+    interface.ena = True
+
+    while True:
+        images = wrapper.get()
+
+        # Process depth to obtain depth over X.
+        depth = images["depth_fac"]
+        depth = cv2.GaussianBlur(depth, (5, 5), 0)
+        depth = depth * mask
+        depth_x = np.max(depth, axis=0)
+        depth_x = np.clip(depth_x / 60, 0, 1)
+
+        # Figure out resulting action.
+        left_fac = np.mean(depth_x[: args.res // 2])
+        right_fac = np.mean(depth_x[args.res // 2 :])
+
+        if left_fac > 0.7 and right_fac > 0.7:
+            # Back out
+            interface.v1 = -1
+            interface.v2 = -1
+            time.sleep(0.5)
+            if left_fac > right_fac:
+                interface.v1 = 0
+            else:
+                interface.v2 = 0
+            time.sleep(0.7)
+
+        elif left_fac > 0.4 or right_fac > 0.4:
+            steer = (left_fac - right_fac) * 5
+            steer = min(1, abs(steer))
+            interface.v1 = 1
+            interface.v2 = 1
+            if left_fac > right_fac:
+                interface.v1 = 1 - steer
+            else:
+                interface.v2 = 1 - steer
+
+        else:
+            interface.v1 = 1
+            interface.v2 = 1
+
+        time.sleep(0.1)
+
+
 def gen_data_main(args, interface):
     dir = Path(args.dir)
     dir.mkdir(exist_ok=True, parents=True)
     data_gen = DataGen(dir)
 
-    if not args.self_rc:
-        interface.add_thread(interface.auto_rc)
-
     pipeline = create_pipeline(args.res)
     print("Setup Depthai pipeline.")
+
     with depthai.Device(pipeline) as device:
         wrapper = PipelineWrapper(device)
 
-        while True:
-            images = wrapper.get()
-
-            if args.self_rc:
-                cv2.imshow("asdf", images["depth_fac"])
-                cv2.waitKey(100)
-
-            if interface.rc_values[5] > 0.5:
-                data_gen.write(images, 1)
-
-            time.sleep(0.01)
+        if args.self_rc:
+            self_rc(args, interface, wrapper, data_gen)
+        else:
+            interface.add_thread(interface.auto_rc)
+            manual_rc(args, interface, wrapper, data_gen)
